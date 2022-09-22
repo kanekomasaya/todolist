9e896561f095d8fbb3a905ad5ad2b2ff593ed340
Namiki-M <m.namiki.carecon@gmail.com>
2022-09-22 20:06:04 +0900

    edit編集

--- code/sample/resources/views/tasks/edit.blade.php
+++ code/sample/resources/views/tasks/edit.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
         <nav class="panel panel-default">
-          <div class="panel-heading">タスクを編集する</div>
+          <div class="panel-heading">タスクを編集しないappia</div>
           <div class="panel-body">
             @if($errors->any())
               <div class="alert alert-danger">
