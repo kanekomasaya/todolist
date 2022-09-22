35de86764e41fe7460ccd5556f1df8fe4ef63d0f
Namiki-M <m.namiki.carecon@gmail.com>
2022-09-22 20:06:45 +0900

    create編集

--- code/sample/resources/views/tasks/create.blade.php
+++ code/sample/resources/views/tasks/create.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
         <nav class="panel panel-default">
-          <div class="panel-heading">タスクを追加する</div>
+          <div class="panel-heading">タスクを追加するaoopi</div>
           <div class="panel-body">
             @if($errors->any())
               <div class="alert alert-danger">
